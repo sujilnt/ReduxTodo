c8f100739fa997f1ce3c6342fbe8bab4cd5151ee
sujilnt <sujilnt@gmail.com>
2018-12-07 23:16:08 +0000

    adding some changes

--- main.js
+++ main.js
@@ -5,25 +5,19 @@ const dataList= document.getElementById("dataList");
 
 const AddTodo = [];
 const dataTodo=[];
-changeFormat = (e)=>{
-	console.log("changeFormat",e);
-	return e.className="checked";
-};
+
 const checkfunc=(e)=>{
 	e.className= e.checked ? "checked": "domValue";
- console.log(e,e.value,e.checked, "clicked");
 };
 
 const deleteFunc = (e)=>{
-	console.log("data list",dataList.innerHTML);
 	dataList.innerHTML="";
 	let data = dataTodo.filter(r=>r!== e.name).join("");
 	dataList.innerHTML=data;
 	
-	
 };
 const checkFunc = (dom)=>{
-	return `<div class="listContainer"><div><input class="checksome" type="checkbox" onchange="checkfunc(this)" value=${dom} ><span class="domValue">${dom}</span></div><button onclick="deleteFunc(this)" name=${dom}>Delete</button></div>` ;
+	return `<li><div class="listContainer"><div><input class="checksome" type="checkbox" onchange="checkfunc(this)" value=${dom} ><span class="domValue">${dom}</span></div><button onclick="deleteFunc(this)" name=${dom}>Delete</button></div></li>` ;
 };
 todoButton.addEventListener("click",(listener)=>{
 	const innertodo = todo.value;
